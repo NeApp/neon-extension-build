a32f57c982d395c21ba5bfb00a302b489be0f12e
Dean Gardiner <me@dgardiner.net>
2018-03-28 10:31:56 +1300

    Fixed formatting issue

--- src/tasks/build/extension.js
+++ src/tasks/build/extension.js
@@ -45,7 +45,7 @@ function registerLinks(browser, environment, rootPath) {
     if(!Filesystem.existsSync(rootPath)) {
         return Promise.resolve();
     }
-    
+
     return Filesystem.readdir(rootPath).then((names) => runSequential(names, (name) => {
         let path = Path.join(rootPath, name);
 
