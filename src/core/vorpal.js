46e9443b0ac577ee5c59039464dc078e1f205120
Dean Gardiner <me@dgardiner.net>
2018-04-16 16:45:50 +1200

    Removed severity level prefix from logger messages

--- src/core/vorpal.js
+++ src/core/vorpal.js
@@ -1,3 +1,4 @@
+import Util from 'util';
 import Vorpal from 'vorpal';
 import VorpalLog from 'vorpal-log';
 
@@ -5,5 +6,24 @@ import VorpalLog from 'vorpal-log';
 const vorpal = Vorpal()
     .use(VorpalLog)
     .delimiter('neon-extension-build$');
+
+function log(msg) {
+    if (typeof msg === 'string') {
+        if (vorpal.logger.options.preformat != null) {
+            msg = vorpal.logger.options.preformat(msg);
+        }
+    } else {
+        msg = Util.inspect(msg);
+    }
+
+    return "" + (vorpal.logger.printDate()) + msg;
+}
+
+// Setup logger formats
+vorpal.logger.addFormatter('debug', 10, log);
+vorpal.logger.addFormatter('info', 20, log);
+vorpal.logger.addFormatter('warn', 30, log);
+vorpal.logger.addFormatter('error', 40, log);
+vorpal.logger.addFormatter('fatal', 50, log);
 
 export default vorpal;
