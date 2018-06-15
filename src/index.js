35f67254236e00a187e66df3c556c1c5fdf6033a
Dean Gardiner <me@dgardiner.net>
2018-06-15 17:47:56 +1200

    Exit the process on unhandled rejections

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import Vorpal from './core/vorpal';
 import './tasks';
 
 
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled rejection for promise:', p);
+    console.log(' - reason:', reason);
+
+    // Exit process
+    process.exit(1);
+});
+
 Vorpal.show().parse(process.argv);
